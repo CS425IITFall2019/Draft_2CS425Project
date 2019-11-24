1875be111ee17d3ed891639359102134bc5b6a01
ClintonGeorge <32846700+ClintonGeorge@users.noreply.github.com>
2019-11-24 16:19:40 -0600

    files added

---
+++ CS425_project/FormTrans.js
@@ -0,0 +1,15 @@
+$(document).ready(function(e) { 
+// I've changed the class name for better reference
+// When someone clicks on 'Sign Up' button
+$('.to_register').on('click', function() {
+  $('.signin_form').css('display', 'none');
+  $('.signup_form').css('display', 'block');
+});
+
+// When someone clicks on 'Sign In' button
+$('.to_login').on('click', function() {
+  $('.signup_form').css('display', 'none');
+  $('.signin_form').css('display', 'block');
+});
+
+});
